e153a6809a68580a3176792d092ac7c958762067
Shaheer-Haq <135428240+Shaheer-Haq@users.noreply.github.com>
2025-03-07 19:58:53 +0500

    Add tests and Dockerfile, update requirements

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,8 @@
-from app import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from app import app
 
 def test_home():
     response = app.test_client().get('/')
